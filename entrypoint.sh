185d16ccea48ffb7cd10f00749adeeb62bae6abb
Vitalii Smolinskyi <vs@crowdin.com>
2020-04-09 23:21:08 +0300

    add download

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,3 +28,33 @@ then
   echo "UPLOAD TRANSLATIONS"
   crowdin upload translations "${config_options[@]}" "${options[@]}"
 fi
+
+
+if [[ "$INPUT_UPLOAD_TRANSLATIONS" = true ]]
+then
+
+  [[ -z "${GITHUB_TOKEN}" ]] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    #exit 1;
+  };
+
+  echo "DOWNLOAD TRANSLATIONS"
+  crowdin download "${config_options[@]}" "${options[@]}"
+
+  echo "CONFIGURATION GIT USER"
+  git config --global user.email "support+bot@crowdin.com"
+  git config --global user.name "Crowdin Bot"
+
+  TRANSLATIONS_BRANCH="crowdin-translations-$(date +%s)"
+
+  git checkout -b ${TRANSLATIONS_BRANCH}
+
+  if [[ -n $(git status -s) ]]
+  then
+      git add .
+      git commit -m "Downloaded translations from Crowdin"
+      git push origin ${TRANSLATIONS_BRANCH}
+  else
+      echo "IT IS CLEAN"
+  fi
+fi
