554c7a6f20c5c73cfd9d59a5d7fee8809484839f
Vitalii Smolinskyi <vs@crowdin.com>
2020-04-09 18:21:39 +0300

    handle inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-set -e
+set -ex
 
-printenv
+echo "STARTING CROWDIN ACTION..."
 
+declare -a config_options=()
+declare -a oprions=( "--no-progress" )
 
-crowdin $1
+if [[ -n "$INPUT_BRANCH_NAME" ]] ; then
+    oprions+=( "--branch=$INPUT_BRANCH_NAME" )
+fi
 
-ls -al
+if [[ "$INPUT_DRYRUN_ACTION" = true ]] ; then
+    oprions+=( "--dryrun" )
+fi
+
+if [[ "$INPUT_UPLOAD_SOURCES" = true ]] ; then
+  echo "UPLOAD SOURCES"
+  crowdin upload sources "${config_options[@]}" "${oprions[@]}"
+fi
+
+if [[ "$INPUT_UPLOAD_TRANSLATIONS" = true ]] ; then
+  echo "UPLOAD TRANSLATIONS"
+  crowdin upload translations "${config_options[@]}" "${oprions[@]}"
+fi
