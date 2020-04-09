44a231251ed95cc7f7f50c49c1700763a6bec6a8
Vitalii Smolinskyi <vs@crowdin.com>
2020-04-09 20:13:22 +0300

    edit entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
 
 echo "STARTING CROWDIN ACTION..."
 
 declare -a config_options=()
-declare -a oprions=( "--no-progress" )
+declare -a options=( "--no-progress" )
 
-if [[ -n "$INPUT_BRANCH_NAME" ]] ; then
-    oprions+=( "--branch=$INPUT_BRANCH_NAME" )
+if [[ -n "$INPUT_BRANCH_NAME" ]]
+then
+    options+=( "--branch=$INPUT_BRANCH_NAME" )
 fi
 
-if [[ "$INPUT_DRYRUN_ACTION" = true ]] ; then
-    oprions+=( "--dryrun" )
+if [[ "$INPUT_DRYRUN_ACTION" = true ]]
+then
+    options+=( "--dryrun" )
 fi
 
-if [[ "$INPUT_UPLOAD_SOURCES" = true ]] ; then
+if [[ "$INPUT_UPLOAD_SOURCES" = true ]]
+then
   echo "UPLOAD SOURCES"
-  crowdin upload sources "${config_options[@]}" "${oprions[@]}"
+  crowdin upload sources "${config_options[@]}" "${options[@]}"
 fi
 
-if [[ "$INPUT_UPLOAD_TRANSLATIONS" = true ]] ; then
+if [[ "$INPUT_UPLOAD_TRANSLATIONS" = true ]]
+then
   echo "UPLOAD TRANSLATIONS"
-  crowdin upload translations "${config_options[@]}" "${oprions[@]}"
+  crowdin upload translations "${config_options[@]}" "${options[@]}"
 fi
